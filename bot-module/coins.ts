076d91dc564af3ecea8618313fe88c54b67da44f
rafi nizar <rafi.nizar15@gmail.com>
2022-04-11 19:46:44 +0700

    coins module

---
+++ bot-module/coins.ts
@@ -0,0 +1,53 @@
+import profileSchema from "../schemas/profile-schema"
+
+interface loose {
+    [key: string]: any
+}
+var coinsCache: loose = {}
+
+export const addCoins = async(guildId: string, userId: string, coins: number) => {
+
+    const result = await profileSchema.findOneAndUpdate({
+        guildId, userId
+    }, {
+        guildId,
+        userId,
+        $inc: {
+            coins
+        }
+    }, {
+        upsert: true,
+        new: true
+    })
+
+    coinsCache[`${guildId}-${userId}`] = result.coins
+
+    return result.coins
+}
+
+export const getCoins = async(guildId: string, userId: string) => {
+    
+    const cachedCoins = coinsCache[`${guildId}-${userId}`]
+
+    if (cachedCoins) return cachedCoins
+
+    let coins = 0
+    const result = await profileSchema.findOne({
+        guildId,
+        userId
+    })
+
+    if (result) {
+        coins = result.coins
+    } else {
+        await new profileSchema({
+            guildId,
+            userId,
+            coins
+        }).save()
+    }
+
+    coinsCache[`${guildId}-${userId}`] = coins
+
+    return coins
+}
